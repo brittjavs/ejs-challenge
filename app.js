e70a2fc7f6dd3e4d4bd9bae423f50aadf6c9097e
Brittany Javalera <brittjavalera@gmail.com>
2020-12-27 14:38:59 -0800

    add get methods for about and contact routes. add header, footer, and paragraph elements to about.ejs and contact.ejs

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.get("/", function(req, res){
   res.render("home", {homeParagraph: homeStartingContent})
 })
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutParagraph: aboutContent})
+})
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactParagraph: contactContent})
+})
 
 
 
